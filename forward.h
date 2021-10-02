02021ee2351b8d6b46d3f8c8034c4442a2438d61
Joseph Blackman <joblac@microsoft.com>
2021-10-02 11:55:38 -0700

    Add comments + a couple more functions + a couple more types

--- forward.h
+++ forward.h
@@ -5,6 +5,8 @@
 using u8 = unsigned char;
 using s8 = signed char;
 using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
 
 enum class Dot    : u8;
 enum class Line   : u8;
